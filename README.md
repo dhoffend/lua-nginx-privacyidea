a2d2b40317c6ea33754cffdb059d2ff646a8b33b
Daniel Hoffend <dh@dotlan.net>
2015-11-27 01:09:13 +0100

    initial tested version for the nginx authentication module

---
+++ README.md
@@ -0,0 +1,58 @@
+LUA based NGINX Authentication plugin for privacyIDEA
+=====================================================
+
+This LUA script enables the nginx webserver to authenticate against
+privacyIDEA using multiple factor authentication like OTP, Yubikey, etc and
+cache the result for a defined lifetime.
+
+Redis is providing the server side cache (for successful HTTP Basic
+Authentications) similar to the privacyIDEA apache2 authentication module.
+
+The lua script only requires the basic nginx-lua, lua-nginx-redis and
+lua-cjson module to be available
+
+Configuration
+-------------
+
+Configuration example nginx virtual host
+
+    location / {
+        # redis host:port
+        # set $privacyidea_redis_host "127.0.0.1";
+        # set $privacyidea_redis_post 6379;
+
+        # how long are accepted authentication allowed to be cached
+        # if expired, the user has to reauthenticate
+        # set $privacyidea_ttl 900;
+
+        # privacyIDEA realm. leave empty == default
+        # set $privacyidea_realm 'somerealm'; # (optional)
+
+        # pointer to the internal validation proxy pass
+        # set $privacyidea_uri "/privacyidea-validate-check";
+
+        # the salt will be used as key/salt for hashing the password
+        # set $privacyidea_salt "some random string as hmac salt";
+
+        # the http realm presented to the user
+        # set $privacyidea_http_realm "Secure zone (use PIN + OTP)";
+
+        access_by_lua_file 'privacyidea.lua';
+   
+    }
+
+    # internal pointer to the validation server
+    # nginx lua support remote calls only via proxy_pass or
+    # by using direct sockect connections
+    location /privacyidea-validate-check {
+        internal;
+        # proxy_ssl_verify off;
+        proxy_pass https://privacyidea/validate/check;
+    }
+    
+Installation
+------------
+
+Tested with Debian Jessie 8.0
+
+    apt-get install nginx-extras lua-nginx-redis lua-cjson redis-server
---
+++ privacyidea.lua
@@ -0,0 +1,179 @@
+-- -- Copyright (C) by Daniel Hoffend (dhoffend)
+
+-- ----------------------------------
+-- extract authentication credentials
+-- ----------------------------------
+
+function extract_credentials()
+    -- Test Authentication header is set and with a value
+    local header = ngx.req.get_headers()['Authorization']
+    if header == nil or header:find(" ") == nil then
+        return false, false
+    end
+
+    local divider = header:find(' ')
+    if header:sub(0, divider-1) ~= 'Basic' then
+        return false, false
+    end
+
+    local auth = ngx.decode_base64(header:sub(divider+1))
+    if auth == nil or auth:find(':') == nil then
+        return false, false
+    end
+
+    local divider = auth:find(':')
+    return auth:sub(0, divider-1), auth:sub(divider+1)
+end
+
+-- ------------------------
+-- Connect to redis storage
+-- ------------------------
+
+function redis_connect(host, port)
+    local redis  = require "nginx/redis"
+    local red = redis:new()
+    red:set_timeout(1000) -- 1 sec
+    local ok, err = red:connect(host, port)
+    if not ok then
+        ngx.log(ngx.ERR, "failed to connect tor redis: ", err)
+        ngx.exit(ngx.HTTP_SERVICE_UNAVAILABLE)
+    end
+    return red
+end
+
+-- ------------------
+-- generate redis key
+-- ------------------
+
+function generate_key(username)
+    return ngx.var.server_name .. ':' .. ngx.var.server_port .. ':' .. ngx.var.document_root .. ':' .. username
+end
+
+-- --------------------------------
+-- password hashing/verify function
+-- --------------------------------
+
+function password_hash(password, salt)
+    -- yes this can be improved by using sha512, bcrypt, etc
+    local digest = ngx.hmac_sha1(salt, password)
+    return ngx.encode_base64(digest)
+end
+
+function password_verify(password, hash, salt)
+    return hash == password_hash(password, salt)
+end
+
+-- -----------------------------------------
+-- validate user credentials with otp server
+-- -----------------------------------------
+
+function privacyidea_validate(username, password)
+
+    -- prepare parameter
+    local params = {user = username, pass = password}
+    local realm = ngx.var.privacyidea_realm or nil
+    if realm then
+        params['realm'] = auth_realm
+    end
+
+    -- send request
+    ngx.req.set_header('Content-Type', 'application/x-www-form-urlencoded')
+    local uri = ngx.var.privacyidea_uri or '/privacyidea-validate-check'
+    local res = ngx.location.capture(uri, {
+        method = ngx.HTTP_POST,
+        body = ngx.encode_args(params)
+    })
+
+    if res.status ~= 200 then
+        return false, 'privacyIDEA HTTP Status ' .. res.status
+    end
+
+    local cjson = require "cjson"
+    local answer = cjson.decode(res.body)
+    local ok = false
+    local err = ''
+    --if res.status ~= 200 and type(answer.result) == "table" then
+    if type(answer.result) == "table" then
+        -- authentication okay
+        if answer.result.status == true then
+            if answer.result.value == true then
+                ok = true
+                return ok, err
+            end
+        end
+
+        -- check for errors
+        if type(answer.detail) == "table" then
+            if answer.detail.message then
+                err = err .. ': ' .. answer.detail.message
+            end
+        end
+        if answer.result.error then 
+            if answer.result.error.message then
+                err = err .. ': ' .. answer.result.error.message
+            end
+        end
+    end
+    return ok, err 
+end
+
+-- --------------------
+-- authentication logic
+-- --------------------
+
+function authenticate()
+    -- Test Authentication header is set and with a value
+    local header = ngx.req.get_headers()['Authorization']
+    if header == nil or header:find(" ") == nil then
+        return false
+    end
+
+    -- extract auth credentials
+    local username, password = extract_credentials()
+    if username == false then
+        return false
+    end
+
+    -- open redis connection    
+    local redis_host = ngx.var.privacyidea_redis_host or '127.0.0.1'
+    local redis_port = ngx.var.privacyidea_redis_port or 6379
+    local ttl = ngx.var.privacyidea_ttl or 900
+    local red = redis_connect(redis_host, redis_port)
+
+    -- lookup key and hash
+    local salt = ngx.var.privacyidea_salt or '1234567890123456'
+    local key = generate_key(username)
+    local value = red:get(key)
+
+    -- password hash ok => extend ttl and return username if status is ok
+    if value and password_verify(password, value, salt) then
+        red:expire(key,ttl)
+        return username
+
+    -- start remote authentication
+    else
+        local ok, err = privacyidea_validate(username, password)
+        if ok then
+            ngx.log(ngx.ERR, '[' .. username .. '] authentication ok')
+            red:setex(key, ttl, password_hash(password, salt))
+            return username
+        else
+            ngx.log(ngx.ERR, '[' .. username .. '] invalid authentication' .. err)
+            return false
+        end
+    end
+end
+
+-- -------------------------
+-- start authentication flow
+-- -------------------------
+
+local user = authenticate()
+
+if not user then
+    local http_realm = ngx.var.privacyidea_http_realm or ''
+    ngx.header.www_authenticate = 'Basic realm="' .. http_realm .. '"'
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    return
+end
+
